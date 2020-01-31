20598cec6279c1d81d8422e2b58725ee7f80483f
Kim <kim@mr-roller.local>
2020-01-31 11:01:55 -0500

    detail page css done

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,6 +1,46 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
+const DetailWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 10px;
+  padding: 20px;
+`;
+const Image = styled.div`
+  margin: 20px;
+  margin-left: 0;
+`;
+const Spec = styled.div`
+  margin-right: 0px;
+`;
+const Desc = styled.div`
+  display: grid;
+  font-weight: 800;
+  div {
+    padding: 5px;
+  }
+`;
+const Desc2 = styled.div`
+  display: grid;
+  font-weight: 900;
+  div {
+    padding: 5px;
+  }
+  margin-left: 10px;
+`;
+const Price = styled.div`
+  background-color: orange;
+  color: white;
+  padding: 5px 5px;
+  text-align: center;
+  font-size: 20px;
+  font-family: "Times New Roman", Times, serif;
+  border: 2px solid orange;
+  border-radius: 10px;
+  width: 50%;
+`;
 class Details extends Component {
   addToCart = () => {
     this.props.addToCart(this.props.model);
@@ -22,25 +62,47 @@ class Details extends Component {
       frontendPath
     } = this.props.model;
     return (
-      <div>
-        <img height="300px" src={frontendPath} />
-        <h1>{model}</h1>
-        <ul>
-          <li>BRAND: {brand}</li>
-          <li>YEAR: {year}</li>
-          <li>PRICE: {price}</li>
-          <li>DISPLACEMENT: {displacement}</li>
-          <li>CYCLINDERS: {cylinders}</li>
-          <li>COOLING SYSTEM: {cooling}</li>
-          <li>POWER: {power}</li>
-          <li>TORQUE: {torque}</li>
-          <li>SEAT HEIGHT: {seatHeight}</li>
-          <li>WEIGHT: {weight}</li>
-          <li>DESCIPTION: {description}</li>
-        </ul>
-        <button onClick={this.addToCart}>Add To Cart</button>
-        <Link to="/">HOME</Link>
-      </div>
+      <DetailWrapper>
+        <Image>
+          <img height="500px" src={frontendPath} />
+        </Image>
+
+        <Spec>
+          <h1>{model}</h1>
+          <Price> {price}$</Price>
+          <DetailWrapper>
+            <Desc>
+              <div>BRAND</div>
+              <div>YEAR</div>
+              <div>DISPLACEMENT</div>
+              <div>CYCLINDERS</div>
+              <div>COOLING</div>
+              <div>POWER</div>
+              <div>TORQUE</div>
+              <div>SEAT HEIGHT</div>
+              <div>WEIGHT</div>
+              <div>DESCIPTION</div>
+            </Desc>
+            <Desc2>
+              <div> {brand}</div>
+              <div> {year}</div>
+              <div> {displacement}cc</div>
+              <div> {cylinders}</div>
+              <div> {cooling}</div>
+              <div> {power}hp</div>
+              <div> {torque}Nm</div>
+              <div> {seatHeight}mm</div>
+              <div> {weight}kg</div>
+              <div> {description}</div>
+            </Desc2>
+          </DetailWrapper>
+          <div>
+            <button onClick={this.addToCart} className="addToCart">
+              Add To Cart
+            </button>
+          </div>
+        </Spec>
+      </DetailWrapper>
     );
   }
 }
